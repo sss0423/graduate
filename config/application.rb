9f74b5bf4de6e120d3a61823c8eab8fa0319540a
sugurusakanushi <suguru9638.soccer@gmail.com>
2019-01-19 07:16:30 +0000

    メール送信設定修正

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module GraduateApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -13,7 +15,7 @@ module GraduateApp
     config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
     config.active_job.queue_adapter = :delayed_job
-    #config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
