0f28bc64e8bfc088dec97ec95a932e5bffec434f
sugurusakanushi <suguru9638.soccer@gmail.com>
2019-01-20 11:15:25 +0000

    mailer修正

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,3 +1,12 @@
-#class DeviseMailer < Devise::Mailer
+class DeviseMailer < ApplicationMailer #Devise::Mailer
 
-#end
+  default from: 'from@example.com'
+
+  def devise_mail(current_user)
+    #@title = picture.title
+    #@content = picture.content
+    mail to: current_user.email , subject: "登録確認"
+  end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          :authentication_keys => [:name]
 
   #nameを必須とする
