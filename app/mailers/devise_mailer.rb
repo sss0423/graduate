d25e51ac71b0f40d6164459a477c116894ebc090
sugurusakanushi <suguru9638.soccer@gmail.com>
2019-01-20 12:07:50 +0000

    mailer修正

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,10 +1,2 @@
-#class DeviseMailer < Devise::Mailer #Devise::Mailer
-
-#  default from: 'from@example.com'
-
-#  def devise_mail(current_user)
-#    mail to: @email , subject: "登録確認"
-#  end
-
-
-#end
+class DeviseMailer < ApplicationMailer 
+end
